f81c601b83a1e8c161b99fa48dfbdc670da64636
Benjamin Carlson <bcarlson336@gmail.com>
2023-03-05 15:21:46 -0700

    I added non-maxima suppression to frame detection

--- utils/onboard.py
+++ utils/onboard.py
@@ -4,12 +4,17 @@ from typing import List, Tuple
 from utils import RectType
 from cv2.dnn import DNN_BACKEND_OPENCV, DNN_BACKEND_CUDA, DNN_TARGET_CPU, DNN_TARGET_CUDA
 
-def setup_model(config_path: str, weights_path: str, use_cuda : bool) -> Tuple[cv2.dnn_Net, List[str]]:
+
+def setup_model(
+    config_path: str, weights_path: str, use_cuda: bool
+) -> Tuple[cv2.dnn_Net, List[str]]:
     """
     Initializes the given DARKNET model
 
+
     :param config_path: The DARKNET configuration path
     :param weights_path: The DARKNET weights path
+    :param use_cuda:  Whether to use CUDA for YOLO execution
     :return: The model network object and the output layer names
     :rtype: Tuple[cv2.dnn_Net, List[str]]
     """
@@ -29,15 +34,21 @@ def setup_model(config_path: str, weights_path: str, use_cuda : bool) -> Tuple[c
 
 
 def detect_frame(
-    bgr_frame: np.ndarray, model: cv2.dnn_Net, layers: List[str], min_conf: float
+    bgr_frame: np.ndarray,
+    model: cv2.dnn_Net,
+    layers: List[str],
+    min_conf: float,
+    NMS_THRESHOLD: float = 0.3,
 ) -> List[Tuple[int, float, RectType]]:
     """
     Detects the objects in a BGR frame using YOLO
+
 
     :param bgr_frame: Frame to detect
     :param model: The YOLO model to use (opencv Net)
     :param layers: Network output layers
     :param min_conf: Confidence threshold
+    :param NMS_THRESHOLD: Non-maxima suppression threshold
     :return List of detected (classID, confidence, RectType) tuples
     """
     h, w = bgr_frame.shape[:2]
@@ -66,4 +77,15 @@ def detect_frame(
 
             return_list.append((classID, confidence, (x, y, width, height)))
 
+    boxes = [box for _, _, box in return_list]
+    confidences = [conf for _, conf, _ in return_list]
+
+    # apply non-maxima suppression to suppress weak, overlapping
+    # bounding boxes
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, min_conf, NMS_THRESHOLD)
+
+    if len(indices) <= 0:
+        return []
+
+    return_list = [return_list[i] for i in indices.flatten()]
     return return_list
